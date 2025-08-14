68e6226a5853fa9d48167974e97d02f3578cfcfd
TENJOU <168693369+tenjou1602@users.noreply.github.com>
2025-08-14 17:58:42 +0800

    Act 4 : DartInputString

--- main.dart
+++ main.dart
@@ -1,28 +1,30 @@
-
-
 import 'dart:io';
 
 void main() {
-  print("Enter Your Name:");
+  print("Enter Your First Name");
   String? name = stdin.readLineSync();
+  
+  if (name != null && name.trim().isNotEmpty) {
+    print("Your First Name: $name");
+  } else {
+    print("Please provide your first name");
+  }
 
-  print("Enter Your Section: ");
-  String? section = stdin.readLineSync();
+  print("Enter Your Last Name");
+  String? last = stdin.readLineSync();
+  
+  if (last != null && last.trim().isNotEmpty) {
+    print("Your Last Name: $last");
+  } else {
+    print("Please provide your last name");
+  }
 
-  print("Enter Your Subject: ");
-  String? subject = stdin.readLineSync();
-
-  print("Enter Your Num1: ");
-  int number1 = int.parse(stdin.readLineSync()!);
-
-  print("Enter Your Num2: ");
-  int number2 = int.parse(stdin.readLineSync()!);
-
-  print("Enter Your Num3: ");
-  double number3 = double.parse(stdin.readLineSync()!);
-
-  print("name: $name, section: $section, subject: $subject");
-  print('numOutput = ${number1 + number2 + number3}');
-  print('numOutput2 = ${number2 - number1 + number3}');
-  print('numOutput3 = ${number3 / number2 * number3}');
+  print("Enter Your Email Address");
+  String? email = stdin.readLineSync();
+  
+  if (email != null && email.trim().isNotEmpty) {
+    print("Your Email: $email");
+  } else {
+    print("Please provide a valid email address");
+  }
 }
